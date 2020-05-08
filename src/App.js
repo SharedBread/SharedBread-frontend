3e0747bb908daa1b47338b7e2bf3d0ec210b0cec
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-08 14:01:29 +0100

    bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Container from "./components/Container"
 
