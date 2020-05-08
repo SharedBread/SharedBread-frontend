a0fea4af7f3ad6cfd43daaf1e7735f53cdaec379
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-08 09:53:07 +0100

    Home route added

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Switch, Route, withRouter } from "react-router-dom";
 
-function Container() {
+// Pages
+import Home from "./Pages/Home";
+
+function Container({ location }) {
   return (
+
+    <Switch location={location}>
+        <Route exact path="/" component={Home} />
+    </Switch>
     
   );
 }
