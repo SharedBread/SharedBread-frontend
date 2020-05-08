3e25b5c0189522a4d28a151482f36cbd9d234681
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-08 10:14:17 +0100

    Router component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
 
 import Container from "./components/Container"
 
 function App() {
   return (
+    <Router>
+
+      <Container /> 
+
+    </Router>
     
     
   );
