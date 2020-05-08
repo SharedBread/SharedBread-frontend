0614fcb3f2561f85e048216dd1a11a983003c259
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-08 14:31:36 +0100

    component update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
 
 import RouteContainer from "./components/RouteContainer"
 
 function App() {
   return (
     <Router>
+      <Container>
+        <RouteContainer /> 
 
-      <RouteContainer /> 
-
+      </Container>
     </Router>
     
     
