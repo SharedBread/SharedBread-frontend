6e8c01904cc137b46de376f06c1637397f36a639
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-09 11:01:29 +0100

    Format applied

--- src/components/FoodBankCarousel.js
+++ src/components/FoodBankCarousel.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { Card, Carousel } from "react-bootstrap";
 
-function FoodBankCarousel () {
-
-    return (
-
-        <Carousel>
-        <Carousel.Item>
-          <Card>
-            <Card.Body>
-              <Card.Title>Food Bank 1</Card.Title>
-              <Card.Text>
-                We're located in Burnley, and require tins of food.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Phone: 01774 482 456</small>
-            </Card.Footer>
-          </Card>
-          </Carousel.Item>
-        <Carousel.Item>
+function FoodBankCarousel() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <Card>
+          <Card.Body>
+            <Card.Title>Food Bank 1</Card.Title>
+            <Card.Text>
+              We're located in Burnley, and require tins of food.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Phone: 01774 482 456</small>
+          </Card.Footer>
+        </Card>
+      </Carousel.Item>
+      <Carousel.Item>
         <Card>
           <Card.Body>
             <Card.Title>Food Bank 2 </Card.Title>
@@ -29,10 +27,9 @@ function FoodBankCarousel () {
             <small className="text-muted">Phone: 01654 482 456</small>
           </Card.Footer>
         </Card>
-        </Carousel.Item>
-      </Carousel>
-        
-    )
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default FoodBankCarousel
+export default FoodBankCarousel;
