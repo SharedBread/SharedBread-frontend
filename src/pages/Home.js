7fc459e272a87a6a1b693dd63edf85deb9a3afd9
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-09 10:58:56 +0100

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, CardDeck } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 function Home() {
   return (
@@ -8,32 +8,6 @@ function Home() {
       <br />
       <h4>Your Local Food Banks</h4>
 
-      <CardDeck>
-        <Card>
-          <Card.Img variant="top" src="" />
-          <Card.Body>
-            <Card.Title>Food Bank 1</Card.Title>
-            <Card.Text>
-              We're located in Burnley, and require tins of food.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Phone: 01774 482 456</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src="" />
-          <Card.Body>
-            <Card.Title>Food Bank 2 </Card.Title>
-            <Card.Text>
-              We're located near the Morrisons in Leyland.
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Phone: 01654 482 456</small>
-          </Card.Footer>
-        </Card>
-      </CardDeck>
       <Button variant="success" block>
         Donate Food
       </Button>
