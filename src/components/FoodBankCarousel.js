ad52e6c0fe67741ea4424843755d9823c08008e1
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-09 11:00:51 +0100

    Carousel structure added with cards

--- src/components/FoodBankCarousel.js
+++ src/components/FoodBankCarousel.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { Button, Card, Carousel } from "react-bootstrap";
+import { Card, Carousel } from "react-bootstrap";
 
 function FoodBankCarousel () {
 
     return (
+
+        <Carousel>
+        <Carousel.Item>
+          <Card>
+            <Card.Body>
+              <Card.Title>Food Bank 1</Card.Title>
+              <Card.Text>
+                We're located in Burnley, and require tins of food.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Phone: 01774 482 456</small>
+            </Card.Footer>
+          </Card>
+          </Carousel.Item>
+        <Carousel.Item>
+        <Card>
+          <Card.Body>
+            <Card.Title>Food Bank 2 </Card.Title>
+            <Card.Text>We're located near the Morrisons in Leyland.</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Phone: 01654 482 456</small>
+          </Card.Footer>
+        </Card>
+        </Carousel.Item>
+      </Carousel>
         
     )
 }
