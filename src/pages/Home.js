97b72b2bd55b3215c033cde0c279d24654cde4ca
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-09 11:08:10 +0100

    Carousel imported

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import FoodBankCarousel from "../components/FoodBankCarousel"
 
 function Home() {
   return (
@@ -7,7 +8,7 @@ function Home() {
       <h1>We will create a UK without the need for food banks</h1>
       <br />
       <h4>Your Local Food Banks</h4>
-
+      <FoodBankCarousel />
       <Button variant="success" block>
         Donate Food
       </Button>
