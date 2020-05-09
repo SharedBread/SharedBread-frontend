a136dcac12acaad3426a986c702288e1ad733877
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-09 13:02:52 +0100

    fetch API & state added

--- src/components/FoodBankCarousel.js
+++ src/components/FoodBankCarousel.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Carousel } from "react-bootstrap";
 
 function FoodBankCarousel(props) {
-  fetch(
-    "https://cors-anywhere.herokuapp.com/https://www.givefood.org.uk/api/1/foodbanks/search/?address=PR253NX"
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  const [location, setLocation] = useState([{ name: "Loading..." }]);
+
+  useEffect(() => {
+    fetch(
+      "https://cors-anywhere.herokuapp.com/https://www.givefood.org.uk/api/1/foodbanks/search/?address=PR253NX"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setLocation(data);
+      });
+  }, []);
 
   return (
     <Carousel>
-      <Carousel.Item>
-        <Card>
-          <Card.Body>
-            <Card.Title>{props.name}</Card.Title>
-            <Card.Text>
-              {props.district}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">{props.phone}</small>
-          </Card.Footer>
-        </Card>
-      </Carousel.Item>
+      {location.map((foodbank) => {
+        return (
+          <Carousel.Item>
+            <Card>
+              <Card.Body>
+                <Card.Title>{foodbank.name}</Card.Title>
+                <Card.Text>{foodbank.district}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">{foodbank.phone}</small>
+              </Card.Footer>
+            </Card>
+          </Carousel.Item>
+        );
+      })}
     </Carousel>
   );
 }
