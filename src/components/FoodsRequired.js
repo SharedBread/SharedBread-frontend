7e6845f599b8162f57ad71d563b164e1836d485d
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-10 17:37:55 +0100

    function added to combine foods needed from API data

--- src/components/FoodsRequired.js
+++ src/components/FoodsRequired.js
@@ -1,13 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 
 function FoodsRequired() {
+  const postcode = "PR253NX"; // this will be dynamic - either geolocation or pc from DB.
 
- 
+  useEffect(() => {
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.givefood.org.uk/api/1/foodbanks/search/?address=${postcode}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        totalRequired(data);
+      });
+  }, []);
 
-  return (
-    <>
-    </>
-  );
+  // function returns an object that shows combined list of foods required from API.
+  const totalRequired = (data) => {
+    const arr = data.map((location) => {
+      return location.needs
+        .toLowerCase()
+        .split("\n")
+        .flat()
+        .map((i) => i.trim());
+    });
+    let frequencyObj = {};
+    arr.flat().forEach((i) => {
+      !frequencyObj[i] ? (frequencyObj[i] = 1) : frequencyObj[i]++;
+    });
+
+    return arr;
+  };
+
+  return <></>;
 }
 
 export default FoodsRequired;
