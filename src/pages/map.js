f6886840e4b8214bb3f5edc23cd78b2e1253e3b1
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-05-10 12:30:43 +0100

    added event handling

--- src/pages/map.js
+++ src/pages/map.js
@@ -15,7 +15,25 @@ function Map() {
         </GoogleMap>
     ));
 
-  
+
+    const  [text, setText] = useState("");
+    const handleTextChange =(event) => {
+        setText(event.target.value);
+    }
+
+    //1- click on the button
+    //2- upon clicking, fetch data from API
+    //3- map the data in a new object and display in the card body
+
+    const handleTextUpdate = (event) => {
+        setLocation(event.target.value)
+        console.log("changed")
+    }
+
+    const handleButtonClicked = (event) => {
+        console.log(event.target.value)
+        console.log("clicked")
+    };
 
     return (
         <div>
@@ -27,10 +45,10 @@ function Map() {
                     mapElement={<div style={{ height: `100%` }} />}
                 />
                 <div className="row">
-                    <div className="col-12 col-md-4"> <input type="test" className="text-control" placeholder="Postcode" />
-                        <button type="Button" class="btn btn-primary"  > Go </button>
+                    <div className="col-12 col-md-4"> <input type="test" className="text-control" placeholder="Postcode" onChange={handleTextChange}/>
+                        <button type="Button" class="btn btn-primary" onClick={handleButtonClicked} > Go </button>
                         <div className="card">
-                            <div> <input type="text" className="form-control"  />
+                            <div> <input type="text" className="form-control" onClick={handleTextUpdate} />
                             </div>
                         </div>
                        
