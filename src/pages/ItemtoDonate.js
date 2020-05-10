a287dbfd288bf595a8cfabf1fa01a3a221cf3f07
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-05-10 19:50:34 +0100

    added input bar ad add button

--- src/pages/ItemtoDonate.js
+++ src/pages/ItemtoDonate.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
-function ItemtoDonate (){
+function ItemtoDonate() {
     return (
         <div>
-           <h1>Item to Donate</h1> 
+            <div>
+                <h1>Item to Donate</h1>
+            </div>
+            <div className="row justify-conent-round">
+                <div> <input class="form-control mr-sm-12" type="search" placeholder="add" aria-label="Search" /></div>
+                <div> <button class="btn btn-outline-success my-2 my-sm-0" type="submit">+</button></div>
+
+            </div>
         </div>
-        
     )
 }
 
 export default ItemtoDonate
+
