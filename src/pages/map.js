87a7787f2c671ec0872a9ae83ba6d61f263a0062
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-05-10 12:27:14 +0100

    added input

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import { GoogleMap,Marker, withGoogleMap} from "react-google-maps"
+import React, { setLocation, useState } from 'react';
+import { GoogleMap, Marker, withGoogleMap } from "react-google-maps";
+import { Button } from "react-bootstrap"
+import { useHistory } from 'react-router-dom';
 
 
-    
+function Map() {
 
-
-function Map(){
-
-const MyMapComponent = withGoogleMap(props =>(
-<GoogleMap
-    defaultZoom={8}
-    defaultCenter={{lat:-34.397, lng:150.644}}
-    >
-        {props.isMarkerShown && <Marker position ={{lat: -34.397, lng: 150.644}}/>}
+    const MyMapComponent = withGoogleMap(props => (
+        <GoogleMap
+            defaultZoom={10}
+            defaultCenter={{ lat: 53.4808, lng: -2.2426 }}
+        >
+            {props.isMarkerShown && <Marker position={{ lat: 53.4808, lng: -2.2426 }} />}
         </GoogleMap>
-));
+    ));
+
+  
+
+
     return (
         <div>
-         <h1>Map Template</h1>  
-        <div>
-      <MyMapComponent
-      containerElement ={<div style={{height: `500px`, width: `500px`}}/>}
-      mapElement={<div style={{height: `100%`}}/>}
-      />
-      
+            <h1>Your Local Food Banks</h1>
+            <div>
+                
+                <MyMapComponent
+                    containerElement={<div style={{ height: `500px`, width: `500px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                />
+                <div className="row">
+                    <div className="col-12 col-md-4"> <input type="test" className="text-control" placeholder="Postcode" />
+        
+                </div>
+            </div>
         </div>
-       </div>
     );
 }
 
