b476c58c9d716e0d0e504e9e9306c24c1bad7027
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-05-12 20:48:47 +0100

    fetched data from API

--- src/pages/NeededItems.js
+++ src/pages/NeededItems.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Card } from 'react-bootstrap';
 
 function NeededItems() {
 
 
-const postCode = "M130LE"
+    const postCode = "M130LE"
 
-const [locations, setLocation] = useState([])
+    const [locations, setLocation] = useState([])
 
     useEffect(() => {
         fetch(`https://cors-anywhere.herokuapp.com/https://www.givefood.org.uk/api/1/foodbanks/search/?address=${postCode}`)
@@ -16,13 +17,26 @@ const [locations, setLocation] = useState([])
     }, []);
 
 
-
-
-
     return (
         <div>
             <h1>Needed Items</h1>
             <div><p>Click + to slect the amount to add to your shopping list</p></div>
+
+            <div className="container">
+            {locations.map(location => {
+                            return (
+                                <Card>
+                                    <Card.Body>
+                                     <Card.Title>  <h4>{location.name} </h4></Card.Title>  
+                                    
+                                      
+                                     <Card.Text>  {location.phone}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            )
+                        })}
+
+            </div>
         </div>
     )
 }
