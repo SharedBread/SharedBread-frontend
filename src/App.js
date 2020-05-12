9569a0abbfb2949826e4ecdd6a319795571f3d41
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-12 23:55:11 +0100

    Authentication components created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import NavBar from "./components/NavBar.js";
 
-import RouteContainer from "./components/RouteContainer"
+import {
+  AmplifyAuthenticator,
+  AmplifySignUp,
+  AmplifyConfirmSignIn,
+  AmplifySignOut,
+} from "@aws-amplify/ui-react";
+
+import RouteContainer from "./components/RouteContainer";
 
 function App() {
   return (
     <div>
-      <Router>
-        <Container>
-          <RouteContainer />
-
-        </Container>
-      </Router>
-      <NavBar />
+      <AmplifyAuthenticator usernameAlias="email">
+        <AmplifySignUp
+          slot="sign-up"
+          usernameAlias="email"
+          formFields={[
+            {
+              type: "email",
+              label: "Enter your email",
+              placeholder: "Enter your email",
+              required: true,
+            },
+            {
+              type: "password",
+              label: "Enter your password",
+              placeholder: "",
+              required: true,
+            },
+            {
+              type: "custom:postcode",
+              label: "Enter your postcode",
+              placeholder: "",
+              required: true,
+            },
+          ]}
+        >
+          <AmplifyConfirmSignIn
+            headerText="Please verify the code sent to your email"
+            slot="confirm-sign-up"
+          />
+        </AmplifySignUp>
+        <AmplifySignOut />
+        <Router>
+          <Container>
+            <RouteContainer />
+          </Container>
+        </Router>
+        <NavBar />
+      </AmplifyAuthenticator>
     </div>
-
-
   );
 }
 
