856d50c58323ecb37c00606c93f28492c9e88524
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-05-12 20:43:28 +0100

    added a postcode constant

--- src/pages/NeededItems.js
+++ src/pages/NeededItems.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 function NeededItems() {
+
+
+    const postCode = "M130LE"
+
+   
+
+
+
+
     return (
         <div>
             <h1>Needed Items</h1>
-        <div><p>Click + to slect the amount to add to your shopping list</p></div>
-      </div>  
+            <div><p>Click + to slect the amount to add to your shopping list</p></div>
+        </div>
     )
 }
 
