fc776948b9c973452b21888ce5015c75ea3f9455
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-14 09:30:12 +0100

    state added

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -3,7 +3,12 @@ import React, { createContext, useState, useEffect } from "react";
 // create initial context to hold the value that gets passed into components
 export const UserContext = createContext(null)
 
-function Auth() {
+// this function is the controller for authorising users and passing
+// the data into the UserContext.
+function AuthProvider() {
+    
+    const [authData, setAuthData] = useState(null)
+    
 
     return (
         <></>
@@ -11,4 +16,4 @@ function Auth() {
 
 }
 
-export default Auth;
+export default AuthProvider;
