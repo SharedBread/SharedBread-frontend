3d2305a918fc3622d54add1a935a30d64a615d0d
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-14 11:09:39 +0100

    AmplifyAuth removed for custom

--- src/App.js
+++ src/App.js
@@ -5,62 +5,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import NavBar from "./components/NavBar.js";
 
-import {
-  AmplifyAuthenticator,
-  AmplifySignUp,
-  AmplifyConfirmSignIn,
-  AmplifySignOut,
-} from "@aws-amplify/ui-react";
 
 import RouteContainer from "./components/RouteContainer";
 
 function App() {
   return (
     <div>
-      <AmplifyAuthenticator usernameAlias="email">
-        <AmplifySignUp
-          slot="sign-up"
-          usernameAlias="email"
-          formFields={[
-            {
-              type: "email",
-              label: "Enter your email",
-              placeholder: "Enter your email",
-              required: true,
-            },
-            {
-              type: "name",
-              label: "Enter your name",
-              placeholder: "First or full name",
-              required: true,
-            },
-            {
-              type: "password",
-              label: "Enter your password",
-              placeholder: "Password should be at least 6 characters",
-              required: true,
-            },
-            {
-              type: "custom:postcode",
-              label: "Enter your postcode",
-              placeholder: "Used for providing local information",
-              required: true,
-            },
-          ]}
-        >
-          <AmplifyConfirmSignIn
-            headerText="Please verify the code sent to your email"
-            slot="confirm-sign-up"
-          />
-        </AmplifySignUp>
-        <AmplifySignOut />
         <Router>
           <Container>
             <RouteContainer />
           </Container>
         </Router>
         <NavBar />
-      </AmplifyAuthenticator>
     </div>
   );
 }
