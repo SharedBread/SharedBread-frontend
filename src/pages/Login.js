fd21c6b1480379528a8b19c290c4cd92ab5f68d1
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-14 11:15:01 +0100

    Login page comp added

---
+++ src/pages/Login.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { AmplifySignUp, AmplifyConfirmSignIn } from "@aws-amplify/ui-react";
+
+function Login() {
+  return (
+    <>
+      <h1>
+        We will create a UK without the need for food banks
+        <span style={{ fontSize: "12px" }}> - The Trussel Trust</span>
+      </h1>
+      <p style={{ marginTop: 30 }}>
+        Thank you for visiting the SharedBread app. To help your local food
+        banks, please sign up below and let's get started.
+      </p>
+      <AmplifySignUp
+        slot="sign-up"
+        usernameAlias="email"
+        formFields={[
+          {
+            type: "email",
+            label: "Enter your email",
+            placeholder: "Enter your email",
+            required: true,
+          },
+          {
+            type: "name",
+            label: "Enter your name",
+            placeholder: "First or full name",
+            required: true,
+          },
+          {
+            type: "password",
+            label: "Enter your password",
+            placeholder: "Password should be at least 6 characters",
+            required: true,
+          },
+          {
+            type: "custom:postcode",
+            label: "Enter your postcode",
+            placeholder: "Used for providing local information",
+            required: true,
+          },
+        ]}
+      >
+        <AmplifyConfirmSignIn
+          headerText="Please verify the code sent to your email"
+          slot="confirm-sign-up"
+        />
+      </AmplifySignUp>
+    </>
+  );
+}
+
+export default Login;
