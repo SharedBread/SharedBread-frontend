06d28c95cbdacbd954aa1fa00c9b2bd8251a5e47
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-14 09:10:15 +0100

    context func created

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,5 +1,8 @@
 import React, { createContext, useState, useEffect } from "react";
 
+// create initial context to hold the value that gets passed into components
+export const UserContext = createContext(null)
+
 function Auth() {
 
     return (
