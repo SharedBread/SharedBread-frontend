8b9a9111b1a1265ec343c311674b4784ee305c1a
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-16 16:06:17 +0100

    comment added

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,4 +1,4 @@
-// File for demo purposes only - SEE DEPLOYMENT BRANCH FOR WORKING COPY
+// For demo purposes only - SEE DEPLOYMENT BRANCH FOR WORKING VERSION.
 
 import React, { createContext, useState, useEffect, useMemo, useContext } from "react";
 import { Auth } from "aws-amplify";
