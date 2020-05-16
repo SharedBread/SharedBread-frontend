0f61bb57f2e86dfc64f9c1695ba193d98df19b89
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-16 16:18:42 +0100

    merge resolved

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-// File for demo purposes only - SEE DEPLOYMENT BRANCH FOR WORKING COPY
-
->>>>>>> master
 import React, { createContext, useState, useEffect, useMemo, useContext } from "react";
 import { Auth } from "aws-amplify";
 
