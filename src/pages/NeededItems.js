dd931d6585654c2c8933a36b1c4c960183db5e50
Hanan Abdimalik <hanan.abdimalok@gmail.com>
2020-05-16 12:13:05 +0100

    implemented event handling

--- src/pages/NeededItems.js
+++ src/pages/NeededItems.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card} from 'react-bootstrap';
 import { GrFormAdd } from 'react-icons/gr';
 
 function NeededItems() {
@@ -13,32 +13,24 @@ function NeededItems() {
         fetch(`https://cors-anywhere.herokuapp.com/https://www.givefood.org.uk/api/1/foodbanks/search/?address=${postCode}`)
             .then((response) => response.json())
             .then((data) => {
-                // console.log(data)
                 setLocation(data);
-                needItems(data);
-
+                console.log(data)
             })
     }, []);
 
-    const needItems = (data) => {
-
-
-        //access object from nested array
-        //display each item separately 
-        //remove unwanted characters 
-        //pass it into the return section as separate items 
-        console.log(data)
+    const addItem = (event) => {
+        console.log(event.target.value)
     }
 
     return (
         <div>
             <h1>Needed Items</h1>
-            <div><p>Click + to slect the amount to add to your shopping list</p></div>
+            <div><p>Click + to slect the item to add to your shopping list</p></div>
 
 
             {locations.map(location => {
                 return (
-                    <div className="container">
+                    <div className="container-fluid">
                         <div className="row">
                             <div className="col-6">
                                 <h5>{location.name} </h5>
@@ -46,18 +38,17 @@ function NeededItems() {
                             </div>
                             <div className="col-6">
                                 <form >
-                                    {location.needs.replace(/And/g,',').replace(/Very Low Stocks/g,',').split(/[,\n/]+/).map(need => {
+                                    {location.needs.replace(/And/g,',').split(/[,\n/]+/).map(need => {
                                             return (
-
-                                                <Card className="form">
-                                                    {need}   <div className="row" className="addFeature" style={{ color: 'white' }} > <GrFormAdd /> </div>
-                                                </Card>
+                                                <Card className="form" style={{ width: '18rem', height: '60%' }}>
+                                                    {need}  
+                                                   
+                                                </Card>    
                                             )
                                         })}
-                                </form>
+                                         <div className="col-1" className="addFeature"> <GrFormAdd onClick={addItem} /> </div>
+                                </form>    
                             </div>
-                            {/* <Button variant="success" > + </Button> */}
-                            {/* <div className="col-2" className="addFeature" > <GrFormAdd/> </div> */}
                         </div>
                         <div className="col-12"> <hr /> </div>
                     </div>
