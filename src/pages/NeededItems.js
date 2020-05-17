68b7b7ed0966f78ca1cef9be6edb1eecdd6ea754
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-17 14:04:38 +0100

    doc formatting applied

--- src/pages/NeededItems.js
+++ src/pages/NeededItems.js
@@ -1,63 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
-import './NeededItems.css';
+import React, { useState, useEffect } from "react";
+import { Card } from "react-bootstrap";
+import "./NeededItems.css";
 
 function NeededItems() {
+  const postCode = "M130LE";
 
+  const [locations, setLocation] = useState([]);
 
-    const postCode = "M130LE"
-
-    const [locations, setLocation] = useState([])
-
-    useEffect(() => {
-        fetch(`https://cors-anywhere.herokuapp.com/https://www.givefood.org.uk/api/1/foodbanks/search/?address=${postCode}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setLocation(data);
-                console.log(data)
-            })
-    }, []);
-
-    const addItem = (event) => {
-        console.log(event.target.value)
-    }
-
-    return (
-        <div style ={{marginBottom: 86}}>
-            <h1>Needed Items</h1>
-            <div><p>Click + to add the item to your shopping list</p></div>
-
-
-            {locations.map(location => {
-                return (
-                    <div className="container">
-                        <div className="row">
-                            < div className="col-4">
-                                <h5>{location.name} </h5>
-                                <p>{location.distance_mi} miles</p>
-                            </div>
-
-                            <div className="col-8">
-                                <form >
-                                    {location.needs.replace(/And/g, ',').split(/[,\n/]+/).map(need => {
-                                        return (
-                                            <Card>
-                                                <div className="form col d-flex justify-content-between">
-                                                    <div className="need" onClick={addItem}> {need} </div>
-                                                    <div className="plus"> + </div>
-                                                </div>
-                                            </Card>
-                                        )
-                                    })}
-                                </form>
-                            </div>
-                            <div className="col"> <hr /> </div>
-                        </div>
-                    </div>
-                )
-            })}
-    </div>  
+  useEffect(() => {
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.givefood.org.uk/api/1/foodbanks/search/?address=${postCode}`
     )
+      .then((response) => response.json())
+      .then((data) => {
+        setLocation(data);
+        console.log(data);
+      });
+  }, []);
+
+  const addItem = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div style={{ marginBottom: 86 }}>
+      <h1>Needed Items</h1>
+      <div>
+        <p>Click + to add the item to your shopping list</p>
+      </div>
+
+      {locations.map((location) => {
+        return (
+          <div className="container">
+            <div className="row">
+              <div className="col-4">
+                <h5>{location.name} </h5>
+                <p>{location.distance_mi} miles</p>
+              </div>
+
+              <div className="col-8">
+                <form>
+                  {location.needs
+                    .replace(/And/g, ",")
+                    .split(/[,\n/]+/)
+                    .map((need) => {
+                      return (
+                        <Card>
+                          <div className="form col d-flex justify-content-between">
+                            <div className="need" onClick={addItem}>
+                              {" "}
+                              {need}{" "}
+                            </div>
+                            <div className="plus"> + </div>
+                          </div>
+                        </Card>
+                      );
+                    })}
+                </form>
+              </div>
+              <div className="col">
+                {" "}
+                <hr />{" "}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default NeededItems;
