63ce76db0e2d0f485b7a276d8e1138d5f0dee4aa
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-19 21:23:01 +0100

    file copy from Hanan branch

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,52 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, Marker, withGoogleMap } from 'react-google-maps';
-import {Card } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import {
+  GoogleMap,
+  Marker,
+  withGoogleMap,
+  withScriptjs,
+} from "react-google-maps";
+import { Card } from "react-bootstrap";
 
 function Map() {
+  const postCode = "PR253NX";
 
-    const MyMapComponent = withGoogleMap(props => (
-        <GoogleMap
-            defaultZoom={8}
-            defaultCenter={{ lat: 55.3781, lng: -3.4360 }}
-        >
-            {props.isMarkerShown && <Marker position={{ lat: 55.3781, lng: -3.4360 }} />}
-        </GoogleMap>
-    ));
+  const [locations, setLocation] = useState([]);
 
-    const postCode = "PR253NX"
+  useEffect(() => {
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.givefood.org.uk/api/1/foodbanks/search/?address=${postCode}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setLocation(data);
+      });
+  }, []);
 
-    const [locations, setLocation] = useState([])
+  const MyMapComponent = withScriptjs(
+    withGoogleMap((data) => (
+      <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: 53.483959, lng: -2.244644 }}
+      >
+        <Marker position={{ lat: 53.483959, lng: -2.244644 }}></Marker>
 
-    useEffect(() => {
-        fetch(`https://cors-anywhere.herokuapp.com/https://www.givefood.org.uk/api/1/foodbanks/search/?address=${postCode}`)
-            .then((response) => response.json())
-            .then((data) => {
-                setLocation(data)
-            })
-    }, []);
+        {locations.map((location) => {
+          return (
+            <Marker
 
-    return (
-        <div>
-            <h1>Your Local Food Banks</h1>
-            <div>
-                <MyMapComponent
-                    containerElement={<div style={{ height: `500px`, width: `100%` }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
-                />
-                        {locations.map(location => {
-                            return (
-                                <Card>
-                                    <Card.Body>
-                                     <Card.Title>  <h4>{location.name} </h4></Card.Title>  
-                                     <Card.Text>  {location.address}</Card.Text>  
-                                     {/* <Card.Text>  {location.postcode}</Card.Text>   */}
-                                     <Card.Text>  {location.phone}</Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            )
-                        })}
-                    </div>
-                </div>
-    );
+            // key={location.postcode}
+            // position={{ lat: location.postcode, lng: location.postcode }}
+            />
+          );
+        })}
+      </GoogleMap>
+    ))
+  );
+
+  return (
+    <div>
+      <h1>Your Local Food Banks</h1>
+      <div>
+        <MyMapComponent
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_API_KEY}`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px`, width: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+        {locations.map((location) => {
+          return (
+            <Card>
+              <Card.Body>
+                <Card.Title>
+                  {" "}
+                  <h4>{location.name} </h4>
+                </Card.Title>
+                <Card.Text> {location.address}</Card.Text>
+                {/* <Card.Text>  {location.postcode}</Card.Text>   */}
+                <Card.Text> {location.phone}</Card.Text>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 export default Map;
