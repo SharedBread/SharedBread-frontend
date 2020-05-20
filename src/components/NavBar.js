b9e7b4640d47ac9173e48c7c927be071d89028b8
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-20 16:32:09 +0100

    ROuter links added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,17 @@ import Nav from 'react-bootstrap/Nav'
 import './NavBar.css';
 import { FaHome, FaMapMarkerAlt, FaRegListAlt } from 'react-icons/fa';
 import { MdAddShoppingCart, MdAccountCircle } from 'react-icons/md';
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
         <>
                 <Nav className="mr-auto navbar row justify-content-center">
-                    <Nav.Link href="/"><FaHome size="2em" /></Nav.Link>
-                    <Nav.Link href="/Map"><FaMapMarkerAlt size="1.8em"/></Nav.Link>
-                    <Nav.Link href="/NeededItems"><FaRegListAlt size="1.9em" /></Nav.Link>
-                    <Nav.Link href="/ItemtoDonate"><MdAddShoppingCart size="2em" /></Nav.Link>
-                    <Nav.Link href="/Profile"><MdAccountCircle size="2.2em" /></Nav.Link>
+                    <Nav.Link><Link to="/"><FaHome size="2em" /></Link></Nav.Link>
+                    <Nav.Link><Link to="/Map"><FaMapMarkerAlt size="1.8em"/></Link></Nav.Link>
+                    <Nav.Link><Link to="/NeededItems"><FaRegListAlt size="1.9em" /></Link></Nav.Link>
+                    <Nav.Link><Link to="/ItemtoDonate"><MdAddShoppingCart size="2em" /></Link></Nav.Link>
+                    <Nav.Link><Link to="/Profile"><MdAccountCircle size="2.2em" /></Link></Nav.Link>
                 </Nav>
         </>
     );
