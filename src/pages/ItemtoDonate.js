fa50fd6eefd2718c61cfb2bba0f35d62426b0d67
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-20 12:53:16 +0100

    useEffect imported

--- src/pages/ItemtoDonate.js
+++ src/pages/ItemtoDonate.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './ItemtoDonate.css';
 import { Button, Card } from "react-bootstrap";
 import { FiTrash } from "react-icons/fi";
