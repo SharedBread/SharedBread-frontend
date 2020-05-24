4f3de14cb68140f1999fd21fdea5e0db2e3d3047
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-24 19:26:27 +0100

    fomatting applied

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,33 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import FoodBankCarousel from "../components/FoodBankCarousel";
-import FoodsRequired from "../components/FoodsRequired"
+import FoodsRequired from "../components/FoodsRequired";
 import { Link } from "react-router-dom";
-import Attribution from "../components/Attribution"
+import Attribution from "../components/Attribution";
 
 function Home() {
   return (
     <>
-      <h1>We will create a UK without the need for food banks<span style={{fontSize:'12px'}}> - The Trussel Trust</span></h1>
+      <h1>
+        We will create a UK without the need for food banks
+        <span style={{ fontSize: "12px" }}> - The Trussel Trust</span>
+      </h1>
       <br />
       <h2>Your Local Food Banks</h2>
       <FoodBankCarousel />
       <FoodsRequired />
-      <div style={{marginTop: 30}}>
-      <Button className="button" variant="success" block>
-      <Link style={{color: 'white'}} to="/NeededItems">Donate Food</Link>
-      </Button>
-      <Button className="button" variant="success" block>
-        Donate Money
-      </Button>
-      <Button className="button" variant="success" block>
-        Volunteer
-      </Button>
+      <div style={{ marginTop: 30 }}>
+        <Button className="button" variant="success" block>
+          <Link style={{ color: "white" }} to="/NeededItems">
+            Donate Food
+          </Link>
+        </Button>
+        <Button className="button" variant="success" block>
+          Donate Money
+        </Button>
+        <Button className="button" variant="success" block>
+          Volunteer
+        </Button>
       </div>
       <Attribution />
     </>
