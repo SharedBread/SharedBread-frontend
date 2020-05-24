2a346e893a18fc28c5fec4e18cec3e9ebf5111ae
Bradley Nichol <bradleynichol@live.co.uk>
2020-05-24 19:27:27 +0100

    Link added & formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import FoodBankCarousel from "../components/FoodBankCarousel";
-import FoodsRequired from "../components/FoodsRequired"
+import FoodsRequired from "../components/FoodsRequired";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
-      <h1>We will create a UK without the need for food banks<span style={{fontSize:'12px'}}> - The Trussel Trust</span></h1>
+      <h1>
+        We will create a UK without the need for food banks
+        <span style={{ fontSize: "12px" }}> - The Trussel Trust</span>
+      </h1>
       <br />
       <h2>Your Local Food Banks</h2>
       <FoodBankCarousel />
       <FoodsRequired />
-      <div style={{marginTop: 30}}>
-      <Button className="button" variant="success" block>
-        Donate Food
-      </Button>
-      <Button className="button" variant="success" block>
-        Donate Money
-      </Button>
-      <Button className="button" variant="success" block>
-        Volunteer
-      </Button>
+      <div style={{ marginTop: 30 }}>
+        <Button className="button" variant="success" block>
+          <Link style={{ color: "white" }} to="/NeededItems">
+            Donate Food
+          </Link>
+        </Button>
+        <Button className="button" variant="success" block>
+          Donate Money
+        </Button>
+        <Button className="button" variant="success" block>
+          Volunteer
+        </Button>
       </div>
     </>
   );
